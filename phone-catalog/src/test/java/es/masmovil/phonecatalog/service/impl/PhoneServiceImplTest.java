791cf148df362444641f5bd2d6f9e1a44ae21f89
skaralyus <skaralyus@n-ix.com>
2018-10-17 20:56:25 +0300

    Fixed tests, added phone-catalog and phone-order services into docker-compose

--- phone-catalog/src/test/java/es/masmovil/phonecatalog/service/impl/PhoneServiceImplTest.java
+++ phone-catalog/src/test/java/es/masmovil/phonecatalog/service/impl/PhoneServiceImplTest.java
@@ -3,7 +3,6 @@ package es.masmovil.phonecatalog.service.impl;
 import es.masmovil.phonecatalog.entity.Phone;
 import es.masmovil.phonecatalog.repository.PhoneRepository;
 import es.masmovil.phonecatalog.util.TestPhoneUtils;
-import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -29,13 +29,11 @@ public class PhoneServiceImplTest {
     @InjectMocks
     private PhoneServiceImpl phoneService;
 
-    @Test
-    public void findByIdReturnNullWhenPhoneDoesNotExists() {
+    @Test(expected = ResponseStatusException.class)
+    public void findByIdReturnBadRequestWhenPhoneDoesNotExists() {
         when(phoneRepository.findPriceById(anyString())).thenReturn(Mono.empty());
 
         final Mono<Phone> phoneById = phoneService.findById("test");
-
-        Assertions.assertThat(phoneById).as("not a null").isNotNull();
 
         Assert.assertNull(phoneById.block());
     }
@@ -47,8 +45,6 @@ public class PhoneServiceImplTest {
 
         final Mono<Phone> phoneExists = phoneService.findById("test");
 
-        Assertions.assertThat(phoneExists).as("not a null").isNotNull();
-
         Assert.assertEquals(phoneExists.block(), expectedPhone);
     }
 
@@ -58,8 +54,6 @@ public class PhoneServiceImplTest {
 
         final List<Phone> phones = phoneService.getPhones(PageRequest.of(0, 5)).collectList().block();
 
-        Assertions.assertThat(phones).as("not a null").isNotNull();
-
         Assert.assertEquals(phones.size(), 0);
     }
 
@@ -68,8 +62,6 @@ public class PhoneServiceImplTest {
         when(phoneRepository.retrieveAllPhones(any())).thenReturn(Flux.just(TestPhoneUtils.createOneTestPhone()));
 
         final List<Phone> phones = phoneService.getPhones(PageRequest.of(0, 5)).collectList().block();
-
-        Assertions.assertThat(phones).as("not a null").isNotNull();
 
         Assert.assertEquals(phones.size(), 1);
         Assert.assertEquals(phones.get(0), TestPhoneUtils.createOneTestPhone());
